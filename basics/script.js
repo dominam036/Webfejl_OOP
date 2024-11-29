260e507ccf52ee53d9751c156328cd8d0bcd3526
dominam036 <https://github.com/dominam036/kerdesek.git>
2024-11-29 15:30:48 +0100

    changed to class from function

--- basics/script.js
+++ basics/script.js
@@ -1,21 +1,47 @@
-function Player( nickName ){
-    this.nickName = nickName;
-    this.playedMatch = 0;
+// function Player( nickName ){
+//     this.nickName = nickName;
+//     this.playedMatch = 0;
 
-}
+// }
 
-Player.prototype.play = function(){
-    this.playedMatch ++;
-    console.log(this.nickName, this.playedMatch);
-}
+// Player.prototype.play = function(){
+//     this.playedMatch ++;
+//     console.log(this.nickName, this.playedMatch);
+// }
 
-Player.prototype.getTierLevel = function(){
-    if( this.playedMatch < 4){
-        return "A"
-    } else if( 3 < this.playedMatch && this.playedMatch < 7 ){
-        return "B"
-    } else {
-        return "C"
+// Player.prototype.getTierLevel = function(){
+//     if( this.playedMatch < 4){
+//         return "A"
+//     } 
+//     else if( 3 < this.playedMatch && this.playedMatch < 7 ){
+//         return "B"
+//     } 
+//     else {
+//         return "C"
+//     }
+// }
+
+class Player{
+    constructor( nickName ){
+        this.nickName = nickName;
+        this.playedMatch = 0;
+    }
+
+    play(){
+        this.playedMatch ++;
+        console.log(this.nickName, this.playedMatch);
+    }
+    
+    getTierLevel(){
+        if( this.playedMatch < 4){
+            return "A"
+        } 
+        else if( 3 < this.playedMatch && this.playedMatch < 7 ){
+            return "B"
+        } 
+        else {
+            return "C"
+        }
     }
 }
 
